f521e7e54b1373db176b581cddee8b3f3a9b0c0d
Dean Harley Pal <paldeanharley@gmail.com>
2025-10-29 22:06:40 +0800

    fix: correct the values for API config variable

--- p3-react-app/src/api/apiConfig.js
+++ p3-react-app/src/api/apiConfig.js
@@ -1,2 +1,2 @@
-const MOVY_API_BASE_URL = import.meta.env.MOVY_API_BASE_URL || "http://localhost:5000/api";
+const MOVY_API_BASE_URL = import.meta.env.VITE_MOVY_API || "http://localhost:5000/api";
 export default MOVY_API_BASE_URL;
