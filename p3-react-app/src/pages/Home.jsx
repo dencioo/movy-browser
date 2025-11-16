776b3ede54f38c8063a4fc128d1466558f252a9c
Dean Harley Pal <paldeanharley@gmail.com>
2025-11-16 23:53:33 +0800

    feat: add initial loading state and fallback UI for Home page content

--- p3-react-app/src/pages/Home.jsx
+++ p3-react-app/src/pages/Home.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from 'react'
 import ScrollTopButton from '../components/ScrollTopButton'
 import NowPlaying from './NowPlaying'
 import Popular from './Popular'
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
 
   const currentDate = new Date().toLocaleDateString(undefined, {
     month: 'long',
     day: 'numeric',
     year: 'numeric',
   })
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 200)
+
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     
@@ -18,12 +28,21 @@ export default function Home() {
           Today is {currentDate}</p>
       </header>
 
-      <section>
-        <NowPlaying />
-      </section>
-      <section>
-        <Popular />
-      </section>
+      {loading ? (
+          <div className="flex justify-center py-10 text-gray-300">
+            Loading...
+          </div>
+        ) : (
+          <>
+            <section>
+              <NowPlaying />
+            </section>
+
+            <section>
+              <Popular />
+            </section>
+          </>
+        )}
 
       <ScrollTopButton />
     </main>
