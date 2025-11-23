40723642e0586535f2ab5069aafd31e3a1524341
Dean Harley Pal <paldeanharley@gmail.com>
2025-11-23 17:41:53 +0800

    feat: add WatchlistCardSkeletion and WatchlistGridLoader components for the laoding state

--- p3-react-app/src/components/Spinner.jsx
+++ p3-react-app/src/components/Spinner.jsx
@@ -27,3 +27,33 @@ export function PageLoader() {
     </div>
   )
 }
+
+export function WatchlistCardSkeleton() {
+  return (
+    <li className='bg-purple-950/50 p-4 rounded-lg border border-purple-700 animate-pulse min-h-[140px] md:min-h-[180px]'>
+      <div className='flex items-center'>
+        <div className='flex-1 text-center space-y-2'>
+          {/* title skeleton*/}
+          <div className='h-8 bg-purple-800/50 rounded w-3/4 mx-auto'></div>
+          {/* movie skeleton*/}
+          <div className='h-5 bg-purple-800/50 rounded w-1/2 mx-auto'></div>
+        </div>
+        {/* action buttons skeleton*/}
+        <div className='flex flex-col gap-2 ml-2'>
+          <div className='w-10 h-10 bg-purple-800/60 rounded-md'></div>
+          <div className='w-10 h-10 bg-purple-800/60 rounded-md'></div>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export function WatchlistGridLoader({ count = 3}) {
+  return (
+    <ul className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+      {Array.from({ length: count }).map((_, i) => (
+        <WatchlistCardSkeleton key={i} />
+      ))}
+    </ul>
+  )
+}
